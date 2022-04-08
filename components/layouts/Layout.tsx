096ffc549ffa51c7e8a28d0003fe44b2908f3929
urreita9 <francisco.urrea9@gmail.com>
2022-04-08 17:04:51 -0300

    first changes

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import Head from 'next/head';
+import { Box } from '@mui/material';
+import { NavBar, SideBar } from '../ui';
+
+interface Props {
+	title?: string;
+	children: React.ReactNode;
+}
+export const Layout: FC<Props> = ({ title = 'Open Jira App', children }) => {
+	return (
+		<Box
+			sx={{
+				flexGrow: 1,
+			}}
+		>
+			<Head>
+				<title>{title}</title>
+			</Head>
+
+			<NavBar />
+
+			<SideBar />
+			<Box sx={{ padding: '10px 20px' }}> {children} </Box>
+		</Box>
+	);
+};
